7aa1f664ed4140ba2eaf4da72ad52db5718484f8
minhHieu2424 <kingbobbb024@gmail.com>
2024-11-04 18:28:10 +0700

    huhuhhh

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
-import { ProductController } from './product/product.controller';
-
 
 @Module({
   imports: [ProductModule],
-  controllers: [AppController,ProductController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ProductModule } from './product/product.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(ProductModule);
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
     options: {
       urls: [process.env.RABBITMQ_URL],
-      queue: 'product_queue',
+            queue: 'product_queue',
       queueOptions: { durable: false },
     },
   });
-  
   await app.startAllMicroservices();
   await app.listen(3002);
 }
-bootstrap();
+bootstrap()
