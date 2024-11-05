8ec641ccda879e5f52f97682b102a9670e3e504d
minhHieu2424 <kingbobbb024@gmail.com>
2024-11-05 12:28:58 +0700

    first cmmmmmommit

--- prisma/generated/product/edge.js
+++ prisma/generated/product/edge.js
@@ -155,7 +155,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
